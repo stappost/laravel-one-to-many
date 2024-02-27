22d24e998e3ca7d84080ca0dfeb028659450fd91
stappost <emanuele.scarpantoni@gmail.com>
2024-02-27 14:35:15 +0100

    add rcmsR and model for type

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\type;
+use App\Http\Requests\StoretypeRequest;
+use App\Http\Requests\UpdatetypeRequest;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoretypeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoretypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(type $type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(type $type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatetypeRequest  $request
+     * @param  \App\Models\type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatetypeRequest $request, type $type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(type $type)
+    {
+        //
+    }
+}
