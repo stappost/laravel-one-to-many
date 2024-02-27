5c6e0af7822962c355e1103122fd5bf135e32ac2
stappost <emanuele.scarpantoni@gmail.com>
2024-02-27 15:01:52 +0100

    show type in details

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <div class="col-12 text-center mt-5">
                 <h2>{{ $project->name }}</h2>
+                <p>{{ $project->type ? $project->type->name : 'Tipo assente' }}</p>
                 <div class="logo">
                     @if ($project->logo != null)
                         <img src="{{ asset('/storage/' . $project->logo) }}" alt="{{ $project->name }}">
